b5c670059f33e132108c41d2deec2f58ff1e899e
Inna3112 <innula3112@gmail.com>
2021-07-22 10:09:45 +0300

    Added reselect library and changed usersSelector

--- src/redux/selectors/users-selectors.ts
+++ src/redux/selectors/users-selectors.ts
@@ -1,9 +1,20 @@
 import {AppStateType} from "../redux-store";
+import {UsersType} from "../users-reducer";
+import {createSelector} from "reselect";
 
+//Простой селектор, нужен для использования в сложном селекторе. Для того, что бы
+// сложному был доступен массив users. Используется первым входящем параметром в сложном
+// селекторе.
 
-export const getUsers = (state: AppStateType) => {
+const getUsersSelector = (state: AppStateType) => {
     return state.usersPage.users
 }
+//Селектор, созданный при помощи библиотеки Reselect, в котором будут применены какие-то
+// сложные вычисления.
+export const getUsers = createSelector(getUsersSelector,
+(users: Array<UsersType>) => {
+    return users.filter(u => true)
+})
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize
 }
