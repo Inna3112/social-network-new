adf7c8f1267625578e899bf444ef313aa91e2563
Inna3112 <innula3112@gmail.com>
2021-08-26 17:03:12 +0300

    profile-reducer fixed

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -76,7 +76,7 @@ export type ProfileActionType = ReturnType<typeof addPost>
 
 const profileReducer = (state = initialState, action: ProfileActionType): ProfilePageType => {
     switch (action.type) {
-        case 'ADD-POST': {
+        case 'samurai-network/profile-page/ADD-POST': {
             const newPost: PostsType = {
                 id: 4,
                 message: action.newPostText,
@@ -88,7 +88,7 @@ const profileReducer = (state = initialState, action: ProfileActionType): Profil
                 // newPostText: ''
             }
         }
-        case "DELETE-POST": {
+        case "samurai-network/profile-page/DELETE-POST": {
             return {
                 ...state,
                 posts: state.posts.filter(p => p.id !== action.id)
@@ -100,14 +100,14 @@ const profileReducer = (state = initialState, action: ProfileActionType): Profil
         //         newPostText: action.newText
         //     }
         // }
-        case 'SET-USER-PROFILE': {
+        case 'samurai-network/profile-page/SET-USER-PROFILE': {
             return {
                 ...state,
                 profile: action.profile,
             }
 
         }
-        case 'SET-STATUS': {
+        case 'samurai-network/profile-page/SET-STATUS': {
             return {
                 ...state,
                 status: action.status
@@ -118,34 +118,34 @@ const profileReducer = (state = initialState, action: ProfileActionType): Profil
     }
 }
 
-export const addPost = (newPostText: string) => ({type: 'ADD-POST', newPostText}) as const
-export const deletePost = (id: number) => ({type: 'DELETE-POST', id}) as const
-export const setUserProfile = (profile: ProfileType) => ({type: 'SET-USER-PROFILE', profile}) as const
-export const setStatus = (status: string) => ({type: 'SET-STATUS', status}) as const
+export const addPost = (newPostText: string) => ({type: 'samurai-network/profile-page/ADD-POST', newPostText}) as const
+export const deletePost = (id: number) => ({type: 'samurai-network/profile-page/DELETE-POST', id}) as const
+export const setUserProfile = (profile: ProfileType) => ({
+    type: 'samurai-network/profile-page/SET-USER-PROFILE',
+    profile
+}) as const
+export const setStatus = (status: string) => ({type: 'samurai-network/profile-page/SET-STATUS', status}) as const
 // export const updateNewPostText = (newText: string) =>
 //     ({type: 'UPDATE-NEW-POST-TEXT', newText: newText}) as const
 
 export const getProfile = (userId: number | null): AppThunk => {
-    return (dispatch, getState) => {
-        usersAPI.getProfile(userId).then(response => {
-            dispatch(setUserProfile(response.data))
-        })
+    return async (dispatch, getState) => {
+        let response = await usersAPI.getProfile(userId)
+        dispatch(setUserProfile(response.data))
     }
 }
 export const getStatus = (userId: number | null): AppThunk => {
-    return (dispatch, getState) => {
-        profileAPI.getStatus(userId).then(response => {
-            dispatch(setStatus(response.data))
-        })
+    return async (dispatch, getState) => {
+        let response = await profileAPI.getStatus(userId)
+        dispatch(setStatus(response.data))
     }
 }
 export const updateStatus = (status: string): AppThunk => {
-    return (dispatch, getState) => {
-        profileAPI.updateStatus(status).then(response => {
-            if(response.data.resultCode === 0){
-                dispatch(setStatus(status))
-            }
-        })
+    return async (dispatch, getState) => {
+        let response = await profileAPI.updateStatus(status)
+        if (response.data.resultCode === 0) {
+            dispatch(setStatus(status))
+        }
     }
 }
 export default profileReducer
