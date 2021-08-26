d94c3a713cd108b6ad5de816e72844c72f3a02a5
Inna3112 <innula3112@gmail.com>
2021-08-26 17:42:28 +0300

    User component added

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {UsersType} from '../../redux/users-reducer';
-import s from './Users.module.css'
-import avaPost from './../../assets/images/avaPost.png'
-import {NavLink} from 'react-router-dom';
-import Paginator from "./Paginator/Paginator";
+import Paginator from './Paginator/Paginator';
+import User from './User/User';
 
 
 type PropsType = {
@@ -17,7 +15,7 @@ type PropsType = {
     unFollow: (userId: number) => void
 }
 
-let Users: React.FC <PropsType> = (props) => {
+let Users: React.FC<PropsType> = (props) => {
 
     let {
         users,
@@ -32,43 +30,15 @@ let Users: React.FC <PropsType> = (props) => {
 
     return <div>
 
-        <Paginator pageSize={pageSize} totalUsersCount={totalUsersCount} currentPage={currentPage} onPageChanged={onPageChanged} />
+        <Paginator pageSize={pageSize} totalUsersCount={totalUsersCount} currentPage={currentPage}
+                   onPageChanged={onPageChanged}/>
 
-        {users.map(u => {
-
-            const onClickFollowHandler = () => {
-                follow(u.id)
-            }
-            const onClickUnFollowHandler = () => {
-                unFollow(u.id)
-            }
-
-            return <div key={u.id} className={s.user}>
-                <div className={s.followingBlock}>
-                    <div>
-                        <NavLink to={'/profile/' + u.id}>
-                            <img src={u.photos.small ? u.photos.small : avaPost}
-                                  alt={'Users photo'} className={s.img} />
-                        </NavLink>
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button disabled={followingInProgress.some(id => id === u.id)} onClick={onClickUnFollowHandler} className={s.button}>Unfollow</button>
-                            : <button disabled={followingInProgress.some(id => id === u.id)} onClick={onClickFollowHandler} className={s.button}>Follow</button>}
-                    </div>
-                </div>
-                <div className={s.content}>
-                    <div className={s.nameAndStatus}>
-                        <div className={s.name}>{u.name}</div>
-                        <div className={s.status}>{u.status}</div>
-                    </div>
-                    <div className={s.location}>
-                        <div className={s.country}>{'u.location.country'}</div>
-                        <div className={s.city}>{'u.location.city'}</div>
-                    </div>
-                </div>
-            </div>
-        })}
+        {users.map(u => <User key={u.id}
+                              user={u}
+                              follow={follow}
+                              unFollow={unFollow}
+                              followingInProgress={followingInProgress}/>
+        )}
     </div>
 }
 
