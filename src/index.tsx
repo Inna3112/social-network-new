0373a817070d5fbc7e70e251d7864efbedcd20fc
Inna3112 <innula3112@gmail.com>
2021-09-04 11:36:26 +0300

    HashRouter added

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/redux-store";
 
 
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
                 {/*store={store} state={store.getState()} dispatch={store.dispatch.bind(store)}*/}
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root'));
 
 
