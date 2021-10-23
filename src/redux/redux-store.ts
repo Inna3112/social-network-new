7f429c174371da5b589690286a1475404db8e5b2
Inna3112 <innula3112@gmail.com>
2021-10-23 18:02:20 +0300

    saga added

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -6,10 +6,34 @@ import usersReducer, {UserActionType} from './users-reducer';
 import authReducer, {AuthActionType} from './auth-reducer';
 import thunkMiddleware, {ThunkAction} from 'redux-thunk'
 import {FormAction, reducer as formReducer} from 'redux-form'
-import appReducer, {AppActionType} from "./app-reducer";
+import appReducer, {AppActionType} from './app-reducer';
+import createSagaMiddleware from 'redux-saga';
+import {all} from 'redux-saga/effects'
 
 
+const rootReducer = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    app: appReducer,
+    form: formReducer,
+})
+const sagaMiddleware = createSagaMiddleware()
 
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware))
+
+sagaMiddleware.run(rootWatcher)
+
+function* rootWatcher() {
+    // yield all([appWatcherSaga(), tasksWatcherSaga()])
+    yield all([])
+}
+// @ts-ignore
+window.store = store
+
+//types
 type RootReducerType = typeof rootReducer //(appState: globalStateType) => appState
 export type AppStateType = ReturnType<RootReducerType>
 export type AllActionsType =
@@ -21,20 +45,3 @@ export type AllActionsType =
     | FormAction
 
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AllActionsType>
-
-
-let rootReducer = combineReducers({
-    profilePage: profileReducer,
-    dialogsPage: dialogsReducer,
-    sidebar: sidebarReducer,
-    usersPage: usersReducer,
-    auth: authReducer,
-    app: appReducer,
-    form: formReducer,
-})
-
-
-export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
-
-// @ts-ignore
-window.store = store
